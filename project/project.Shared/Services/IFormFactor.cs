b2d08ba30daa022a82cc69865691537660695538
Georaldo <qwertytwerk2003@gmail.com>
2026-02-03 10:33:40 +0800

    first commit

---
+++ project/project.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace project.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ project/project/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using project.Shared.Services;
+
+namespace project.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return DeviceInfo.Idiom.ToString();
+        }
+
+        public string GetPlatform()
+        {
+            return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+        }
+    }
+}
